465ff64c9377fc707c7860aa4cc6afa8becbe328
Roger Moore <rmoore74@live.co.uk>
2016-09-15 10:33:35 +0100

    Added json-prettifier project to projects page

--- public/js/controllers/ProjectCtrl.js
+++ public/js/controllers/ProjectCtrl.js
@@ -2,7 +2,26 @@ angular.module('ProjectCtrl', []).controller('ProjectController', function($scop
 
     $scope.projectList = [
         {
-            lightId: 'project-1',
+            lightId: 'project-9',
+            title: 'json-prettifier',
+            description: '\'json-prettifier\' is a recent project that I have been working on. It is a basic, barebones JSONLint clone. Basically, I love JSONLint and I remember a time when it went down for a while and it was almost as though it was the end of the world, so I decided it would be a  fun project to take up myself. I have recently fallen in love with the MEAN stack (MongoDB, ExpressJS, AngularJS, and NodeJS), thus this project is based off of that stack (as is this very website). The source code for this can be found on my GitHub.',
+            pictures: [
+                {
+                    thumbUrl: '/img/projects/json-prettifier/main.png',
+                    imgUrl: '/img/projects/json-prettifier/main.png'
+                },
+                {
+                    thumbUrl: '/img/projects/json-prettifier/invalid.png',
+                    imgUrl: '/img/projects/json-prettifier/invalid.png',
+                },
+                {
+                    thumbUrl: '/img/projects/json-prettifier/valid.png',
+                    imgUrl: '/img/projects/json-prettifier/valid.png'
+                }
+            ]
+        },
+        {
+            lightId: 'project-8',
             title: 'Dropshipping DB Manager',
             description: 'This project involved creating a database item manager for a dropshipping venture that I was working on in the past. The main technologies used here for this project are PHP and SQL. Mainly because these work on the server that we were using to host the website was based on the traditional LAMP stack. The main functionality of the dropshipping item database manager is searching, adding, editing, and removing products etc.',
             pictures: [
@@ -33,7 +52,7 @@ angular.module('ProjectCtrl', []).controller('ProjectController', function($scop
             ]
         },
         {
-            lightId: 'project-2',
+            lightId: 'project-7',
             title: 'Magic Link Cloaker',
             description: 'This was a fun little project that I worked on. Basically, the idea was to create a URL shortener to make ugly affiliate links (e.g., internet marketing) more bareable to look at. So in principle, it was similar to something like TinyURL. However, the idea here was that you could install the script on your website and then the links would be cloaked from your URL. So, you could mask an affiliate link and just give the user a link from your site which would then redirect to the affiliated product. The source for this can be found on my GitHub.',
             pictures: [
@@ -64,7 +83,7 @@ angular.module('ProjectCtrl', []).controller('ProjectController', function($scop
             ]
         },
         {
-            lightId: 'project-3',
+            lightId: 'project-6',
             title: 'Race Data Application',
             description: 'This is a project that I created for my programming module at King’s College. This was part of my second coursework in the module. This application pulls data from an online database, using a jar file that our tutor provided us with, which allows you to choose athletes and add an athlete friend to the main list. You can add or remove these friends. When you double click on a friend it will take you to their race results. You can also tweet race results to your Twitter account and you can view a graphical representation of the results, via charts. To create this project I used Java, the Java Swing library, the Twitter4j library, and the JFreeChart library. This was a fun and challenging piece of coursework.',
             pictures: [
@@ -99,7 +118,7 @@ angular.module('ProjectCtrl', []).controller('ProjectController', function($scop
             ]
         },
         {
-            lightId: 'project-4',
+            lightId: 'project-5',
             title: 'Android MD5 Checksum App',
             description: 'Okay, so visually this application doessn’t look too great at the moment. This is because this app is currently a work in progress. To expand my skills in Android development, I set myself the task of creating an application that allows the user to choose a file on their phone and generate its unique MD5 hash. I also provide the option to convert plain text strings into MD5 as well. When the MD5 is generated the hash is automatically copied to clipboard and an Android toast is displayed to alert the user of this. I just have to get file checksum generation working and then sort out the user interface until I have completed this project.',
             pictures: [
@@ -114,7 +133,7 @@ angular.module('ProjectCtrl', []).controller('ProjectController', function($scop
             ]
         },
         {
-            lightId: 'project-5',
+            lightId: 'project-4',
             title: 'First Person Messabout',
             description: 'This is a project I created whilst working on ‘Unit 22 – Developing Computer Games’, whilst I was at college. For this project, the rest of my class were using Blender to design and create their games, but I wanted to challenge myself and I asked my tutor if I was able to create the game in something other than Blender. I ended up using C++ with a game development library. The library that I used was DarkGDK. I had no experience with DarkGDK and limited experience with C++, so I had to get comfortable with C++ again as well as learning a new library. As I was developing the game from scratch using hard code, I made use of models that I had found off of the internet (this was allowed as long I referenced all external sources, which I did). Within my limited timescale I was able to create a game where you were able to roam the map freely. The game included physics, allowing the user to jump up and down and fall off of objects etc. Collision detection was also working, so the user couldn’t just walk through walls. The user moved around using W, A, S, and D keys, with spacebar allocated for jumping.',
             pictures: [
@@ -149,7 +168,7 @@ angular.module('ProjectCtrl', []).controller('ProjectController', function($scop
             ]
         },
         {
-            lightId: 'project-6',
+            lightId: 'project-3',
             title: 'X Ercise Booking Application',
             description: 'This was a project that I was assigned whilst I was at Bexhill College. For one of my college assignments I had to create a booking form application for a gym. This application would allow users to book equipment in the gym for a certain date at a certain time. Users were also able to view bookings that had already been made. Bookings were stored in an external database. I used VB.NET to create this application.',
             pictures: [
@@ -168,7 +187,7 @@ angular.module('ProjectCtrl', []).controller('ProjectController', function($scop
             ]
         },
         {
-            lightId: 'project-7',
+            lightId: 'project-2',
             title: 'Enigma Machine',
             description: 'This is another project that I created in VB.NET. This was a project set to me by my tutor at Bexhill College. It was a really fun and exciting project and pushed me to my limits of skill, at the time. This is an application that simulates the famous Enigma machine which was used by German military to send encoded messages to each other. These transmissions were famously cracked by Alan Turing!',
             pictures: [
@@ -179,7 +198,7 @@ angular.module('ProjectCtrl', []).controller('ProjectController', function($scop
             ]
         },
         {
-            lightId: 'project-8',
+            lightId: 'project-1',
             title: 'Veloci Games',
             description: 'This is a website that my friend and I designed and created for free to help out a freelance game designer. In the end, the game designer didn’t use the website that we created, but we were proud of what we had created. This website is rather dated and was created back in 2010 when we were quite young. The site included HTML, CSS, PHP (used for the Twitter feed), and jQuery.',
             pictures: [
